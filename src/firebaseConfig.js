f070b91267daf0ee5c108b7fb8248402000ae5d1
IldizovNodirbek <158564155+IldizovNodirbek@users.noreply.github.com>
2025-05-26 08:36:16 +0500

    changed redirect-url

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -18,15 +18,5 @@ const auth = getAuth(app);
 export { auth };
 
 
-// new connfig 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDw_g_yHf93ke01whUtX9hAue6tfIOr-aI",
-//   authDomain: "daily-task-2b818.firebaseapp.com",
-//   projectId: "daily-task-2b818",
-//   storageBucket: "daily-task-2b818.firebasestorage.app",
-//   messagingSenderId: "788767915642",
-//   appId: "1:788767915642:web:abff1e6551c538c37c85c3",
-//   measurementId: "G-R749ZHPN01"
-// };
 
 
